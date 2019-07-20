1a90f4803c728f5748fc769f930f56c4afbe15a0
tamura <tamura2004@gmail.com>
2019-07-21 07:40:30 +0900

    Comment measure

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const SIZE = 88;
 const WIDTH = 11;
 const HEIGHT = 15;
-const NAMES = ['ヴィ', 'レン', 'ロジ', 'ハッ', 'パイ'];
+// const NAMES = ['ヴィ', 'レン', 'ロジ', 'ハッ', 'パイ'];
 
 let locked = false;
 let tiles = [];
@@ -39,18 +39,18 @@ class Ball {
     strokeWeight(3);
     ellipse(this.p.x, this.p.y, SIZE - 10);
   }
-  drawName() {
-    textSize(32);
-    fill(255);
-    textStyle(BOLD);
-    stroke(0);
-    strokeWeight(6);
-    textAlign(CENTER);
-    text(NAMES[this.id], this.p.x - SIZE / 2, this.p.y + 14, SIZE);
-  }
+  // drawName() {
+  //   textSize(32);
+  //   fill(255);
+  //   textStyle(BOLD);
+  //   stroke(0);
+  //   strokeWeight(6);
+  //   textAlign(CENTER);
+  //   text(NAMES[this.id], this.p.x - SIZE / 2, this.p.y + 14, SIZE);
+  // }
   draw() {
     this.drawFrame();
-    this.drawName();
+    // this.drawName();
   }
   collid(x, y) {
     return collid(x, y, this.p.x - SIZE / 2, this.p.y - SIZE / 2, SIZE);
@@ -69,21 +69,21 @@ function setup() {
   }
 }
 
-function drawMeasure() {
-  const x = target.p.x;
-  const y = target.p.y;
-  const mx = mouseX;
-  const my = mouseY;
-  const tx = (x + mx) / 2;
-  const ty = (y + my) / 2;
-  const d = dist(x, y, mx, my);
-  line(x, y, mx, my);
-  textSize(64);
-  fill(250);
-  stroke(0);
-  strokeWeight(4);
-  text(`${int(d / SIZE) * 5}feet`, tx, ty);
-}
+// function drawMeasure() {
+//   const x = target.p.x;
+//   const y = target.p.y;
+//   const mx = mouseX;
+//   const my = mouseY;
+//   const tx = (x + mx) / 2;
+//   const ty = (y + my) / 2;
+//   const d = dist(x, y, mx, my);
+//   line(x, y, mx, my);
+//   textSize(64);
+//   fill(250);
+//   stroke(0);
+//   strokeWeight(4);
+//   text(`${int(d / SIZE) * 5}feet`, tx, ty);
+// }
 
 function draw() {
   background(220);
@@ -95,7 +95,7 @@ function draw() {
   if (locked && target) {
     fill(128, 128, 128, 0);
     ellipse(mouseX, mouseY, SIZE - 10);
-    drawMeasure();
+    // drawMeasure();
   }
   for (const ball of balls) {
     ball.move();
